cf458b231cf9ba44531fa082b92c0122c7db5f14
fi4n_cozydev <fian.tk@gmail.com>
2024-04-06 14:06:30 +0700

    devp

--- 04-package/modules-example/request.js
+++ 04-package/modules-example/request.js
@@ -1,1 +1,28 @@
 const axios = require('axios');
+
+axios.get('https://www.wiki.com')
+    .then( (response) => {
+        console.log(response);
+    })
+    .catch( (err) => {
+        console.log(err);
+    })
+    .then( () => {
+        console.log('[SERVER] :> ALL DONE!');
+    });
+
+/*
+    node request.js
+    npm start
+    -----------------------------------------------------------
+    [ NodeMon : automatic run after change something in file ]
+    npm run dev 
+    ./node_modules/.bin/nodemon request.js
+
+    [ GLOBAL INSTALL ] > recommended to do
+    npm install -g nodemon
+
+    nodemon request.js
+
+    npm uninstall nodemon
+*/
