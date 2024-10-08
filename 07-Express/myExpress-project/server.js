74e6e8f5c806bf91f8ec347b1a29149d93d162c3
fi4n_cozydev <fian.tk@gmail.com>
2024-10-08 19:51:43 +0700

    Update server.js

--- 07-Express/myExpress-project/server.js
+++ 07-Express/myExpress-project/server.js
@@ -8,6 +8,15 @@ const friends = [
     { id: 2, name: 'Samurai' }
 ];
 
+//added middleWere
+app.use((req, res, next) => {
+    const start = Date.now();    
+    next();
+    //all process along the the queue actions here...then >>
+    const delta = Date.now() - start;
+    console.log(`${req.method} \t ${req.url} \t ${delta} milliSeconds`);
+});
+
 app.get ('/', (req, res) => {
     res.send('Hello Express!')
     // res.send({ id: 1, name: 'Tommy' });
@@ -15,6 +24,29 @@ app.get ('/', (req, res) => {
 
 app.get ('/friends', (req, res) => {
     res.json(friends);
+});
+
+//parse JSON from middleWere
+app.use(express.json());
+
+//make an endPoint
+app.post('/friends', (req, res) => {
+    //validation of data type to ensure that user put a correct data not an Oject
+    //**HTTP response status code
+    //?[1] information 100~199 [2] successful 200~299 [3] redirects 300~399
+    //?[4] client errors 400~499  [5] server errors 500~599
+    if (!req.body.name) {       //to make sure that this field NOT-Empty
+        return res.status(400).json({
+            error: 'server unacceptable for this type of data.'
+        });
+    };
+
+    const addNewFriend = {
+        name: req.body.name,
+        id: friends.length      //get a new ID by tracking ARRAY length (auto)
+    };
+    friends.push(addNewFriend); // Add new Data
+    res.json(addNewFriend);
 });
 
 app.get('/friends/:id', (req, res) => { //get specific data by ID like "GET /friends/1"
