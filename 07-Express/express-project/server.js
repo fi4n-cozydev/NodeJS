a80230a9d189e8270a76990b0e4d1712c3ac36a0
fi4n_cozydev <fian.tk@gmail.com>
2024-10-08 12:40:08 +0700

    ีupdate 07sec

--- 07-Express/express-project/server.js
+++ 07-Express/express-project/server.js
@@ -1,60 +1,36 @@
 const express = require('express');
+const path = require('path');
+
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 const PORT = 3000;
 
-const friends = [
-    {id: 0, name: 'Albert'}, {id: 1, name: 'Steep'}, {id: 2, name: 'Johnny'}
-];
-
-//** Middleware */
 app.use((req, res, next) => {
-    const start = Date.now();    
-    next();
-    //actions go here...
-    const delta = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${delta}ms`);    
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+app.use('/site', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-//** SET ROUT Handler */
-/*
 app.get('/', (req, res) => {
-    // res.send('Hello.....');
-    res.send({
-        id: 1, name: 'Johnny'
-    });
-});
-*/
-
-app.get('/friends', (req, res) => {
-    res.json(friends);
+  res.render('index', {
+    title: 'My Friends Are VERYY Clever',
+    caption: 'Let\'s go skiing!',
+  });
 });
 
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if(friend) {
-        // res.json(friend);
-        res.status(200).json(friend);
-    } else {
-        // res.sendStatus(404);
-        res.status(404).json({
-            error: 'Friend does not exit'
-        });
-    }
-})
-// http://localhost:3000/friends/3 -> result -> {error "Friend does not exit"}
-
-app.get('/messages', (req, res) => {
-    res.send('<ul><li>Hello..., cozyDev!</li></ul>');
-});
-
-app.post('/messages', (req, res) => {
-    res.send('Updating messages...');
-});
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`);
+  console.log(`Listening on ${PORT}...`);
 });
