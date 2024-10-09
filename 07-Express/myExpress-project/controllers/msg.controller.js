1de160e0af8a8cd6610cab6c62fa5ca695745d56
fi4n_cozydev <fian.tk@gmail.com>
2024-10-09 12:23:03 +0700

    add send File type image

--- 07-Express/myExpress-project/controllers/msg.controller.js
+++ 07-Express/myExpress-project/controllers/msg.controller.js
@@ -1,6 +1,14 @@
+const path = require('path');
+//* macOS /folder/image_file.jpg
+//* window \folder\image_file.jpg
+
 function getMsg(req, res) {
-    const greeting = '<h1>Welcome!</h1>';
-    res.send(greeting);
+    // const greeting = '<h1>Welcome!</h1>';
+    // res.send(greeting);
+
+    //!Image File handler    
+    // res.sendFile('image_file.jpg');
+    res.sendFile(path.join(__dirname, '..', 'public', 'smile.png'));
 }
 
 function postMsg(req, res) {
