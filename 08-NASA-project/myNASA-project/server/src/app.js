e7a685c8e01822da615949943b72ea416c28d6df
fi4n_cozydev <fian.tk@gmail.com>
2024-10-15 00:02:02 +0700

    added launched model

--- 08-NASA-project/myNASA-project/server/src/app.js
+++ 08-NASA-project/myNASA-project/server/src/app.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -9,8 +11,15 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
+
 // middleware chains
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
