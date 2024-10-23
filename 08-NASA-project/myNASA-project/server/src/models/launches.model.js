cb3689ec986e0513a41862f349b9f689f840b685
fi4n_cozydev <fian.tk@gmail.com>
2024-10-23 15:54:13 +0700

    upadating

--- 08-NASA-project/myNASA-project/server/src/models/launches.model.js
+++ 08-NASA-project/myNASA-project/server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
     rocket: "Explorer IS1",
     launchDate: new Date('December 27, 2030'),
     destination: 'Kepler-442 b',
-    customer: ['ZTM', NASA],
+    customer: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
 };
