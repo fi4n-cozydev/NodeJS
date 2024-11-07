2f73d08c120a12f7e2543c1d96a6756501b41a0a
fi4n_cozydev <fian.tk@gmail.com>
2024-11-07 22:43:24 +0700

    setup MongoDB + mongoose

---
+++ 08-NASA-project/MongoDB.md
@@ -0,0 +1,5 @@
+# Database User
+# ID: nasa-api
+# PWD: VFjwn6ZmfQ1okly3
+
+# application-code: mongodb+srv://nasa-api:<db_password>@nasacluster.sjuaw.mongodb.net/?retryWrites=true&w=majority&appName=NasaCluster
--- 08-NASA-project/myNASA-project/server/src/server.js
+++ 08-NASA-project/myNASA-project/server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,20 @@ const {loadPlanetData} = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:VFjwn6ZmfQ1okly3@nasacluster.sjuaw.mongodb.net/nasa?retryWrites=true&w=majority&appName=NasaCluster';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('err');
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetData();
 
     server.listen(PORT, () => {
